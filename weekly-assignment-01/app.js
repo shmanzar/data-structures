1d4dcca6a81d5519f8f8d81e1807fa695dd305e9
shmanzar <email@smanzar.com>
2020-09-03 18:35:56 -0400

    Succesfully completed Assignment 1 task | Documentation remaining!

---
+++ weekly-assignment-01/app.js
@@ -0,0 +1,33 @@
+// npm install request
+// mkdir data
+
+var request = require('request');
+var fs = require('fs');
+
+
+var link_array = [];
+
+for (let i = 0; i <= 10; i++) {
+	link_array.push('https://parsons.nyc/aa/m0'+i+'.html')
+	if (link_array[i] === 'https://parsons.nyc/aa/m010.html') {
+		link_array[i] ='https://parsons.nyc/aa/m10.html' 
+
+}
+}
+
+
+var filtered_array = link_array.filter(function(value, index, arr){ return value != 'https://parsons.nyc/aa/m00.html'})
+
+function getData(link) {
+	request(link, function(error, response, body){
+    if (!error && response.statusCode == 200) {
+       	 let filename = link.match(/m\d{2}/g)	
+	 fs.writeFileSync('data/'+filename+'.txt', body);
+	 console.log("Files downloaded!")
+    }
+    else {console.log("Request failed!")}})
+}
+
+filtered_array.forEach(link => getData(link));
+
+
