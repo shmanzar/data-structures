585c116ebadd62cc6a4b275ae810e3e960f015df
shmanzar <manzs324@newschool.edu>
2020-12-02 03:36:26 +0000

    fixed w12 | AWS responding better | some space has been recovered

--- weekly-assignment-11/app.js
+++ weekly-assignment-11/app.js
@@ -6,10 +6,10 @@ const moment = require('moment-timezone');
 const handlebars = require('handlebars');
 var fs = require('fs');
 const dotenv = require('dotenv');
-dotenv.config({});
+dotenv.config();
 
-// const indexSource = fs.readFileSync("templates/sensor.txt").toString();
-// var template = handlebars.compile(indexSource, { strict: true });
+const indexSource = fs.readFileSync("templates/sensor.txt").toString();
+var template = handlebars.compile(indexSource, { strict: true });
 
 // const pbSource = fs.readFileSync("templates/pb.txt").toString();
 // var pbtemplate = handlebars.compile(pbSource, { strict: true });
@@ -17,7 +17,7 @@ dotenv.config({});
 // AWS RDS credentials
 var db_credentials = new Object();
 db_credentials.user = process.env.AWSRDS_UN;
-db_credentials.host = process.env.AWSRDS_HT;
+db_credentials.host = process.env.AWSRDS_HOST;
 db_credentials.database = process.env.AWSRDS_DB;
 db_credentials.password = process.env.AWSRDS_PW;
 db_credentials.port = process.env.AWSRDS_PORT;
@@ -39,42 +39,42 @@ var hx = `<!doctype html>
 </head>
 <body>
 <h1>AA data endpoint</h1>
-<div id='map' style='width: 700px; height: 600px;'></div>
-// <div id="mapid"></div>
-// <script src="https://unpkg.com/leaflet@1.6.0/dist/leaflet.js"
-//   integrity="sha512-gZwIG9x3wUXg2hdXF6+rVkLF/0Vi9U8D2Ntg4Ga5I5BZpVkVxlJWbSQtXPSiUTtC0TjtGOmxa1AJPuV0CPthew=="
-//   crossorigin=""></script>
+// <div id='map' style='width: 700px; height: 600px;'></div>
+<div id="mapid"></div>
+<script src="https://unpkg.com/leaflet@1.6.0/dist/leaflet.js"
+  integrity="sha512-gZwIG9x3wUXg2hdXF6+rVkLF/0Vi9U8D2Ntg4Ga5I5BZpVkVxlJWbSQtXPSiUTtC0TjtGOmxa1AJPuV0CPthew=="
+  crossorigin=""></script>
   <script>
   var data = 
   `;
 
 var jx = `;
-            mapboxgl.accessToken = 'pk.eyJ1Ijoic21hbnphciIsImEiOiJja2k2ajRjaWowMXEyMnFxZ2IxbTRhaDkwIn0.bZyOlzap-1dfxKN_BHcCPw';
-        var map = new mapboxgl.Map({
-            container: 'map',
-            style: 'mapbox://styles/smanzar/cki6j8dxf3o2p19trqspv7hoh'
-        });
-    </script>
-    
-    // var mymap = L.map('mapid').setView([40.734636,-73.994997], 13);
-    // L.tileLayer('https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token={accessToken}', {
-    // attribution: '© <a href="https://www.mapbox.com/about/maps/">Mapbox</a> © <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a> <strong><a href="https://www.mapbox.com/map-feedback/" target="_blank">Improve this map</a></strong>',
-    // tileSize: 512,
-    // maxZoom: 18,
-    // zoomOffset: -1,
-    // id: 'mapbox/streets-v11',
-    //     accessToken: 'pk.eyJ1Ijoidm9ucmFtc3kiLCJhIjoiY2pveGF1MmxoMjZnazNwbW8ya2dsZTRtNyJ9.mJ1kRVrVnwTFNdoKlQu_Cw'
-    // }).addTo(mymap);
-    // for (var i=0; i<data.length; i++) {
-    //     L.marker( [data[i].lat, data[i].lon] ).bindPopup(JSON.stringify(data[i].meetings)).addTo(mymap);
-    // }
+    //         mapboxgl.accessToken = 'pk.eyJ1Ijoic21hbnphciIsImEiOiJja2k2ajRjaWowMXEyMnFxZ2IxbTRhaDkwIn0.bZyOlzap-1dfxKN_BHcCPw';
+    //     var map = new mapboxgl.Map({
+    //         container: 'map',
+    //         style: 'mapbox://styles/smanzar/cki6j8dxf3o2p19trqspv7hoh'
+    //     });
     // </script>
+    
+    var mymap = L.map('mapid').setView([40.734636,-73.994997], 13);
+    L.tileLayer('https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token={accessToken}', {
+    attribution: '© <a href="https://www.mapbox.com/about/maps/">Mapbox</a> © <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a> <strong><a href="https://www.mapbox.com/map-feedback/" target="_blank">Improve this map</a></strong>',
+    tileSize: 512,
+    maxZoom: 18,
+    zoomOffset: -1,
+    id: 'mapbox/streets-v11',
+        accessToken: 'pk.eyJ1Ijoic21hbnphciIsImEiOiJja2k2ajRjaWowMXEyMnFxZ2IxbTRhaDkwIn0.bZyOlzap-1dfxKN_BHcCPw'
+    }).addTo(mymap);
+    for (var i=0; i<data.length; i++) {
+        L.marker( [data[i].lat, data[i].lon] ).bindPopup(JSON.stringify(data[i].meetings)).addTo(mymap);
+    }
+    </script>
     </body>
     </html>`;
 
 
 app.get('/', function(req, res) {
-    res.send('<h3>Code demo site</h3><ul><li><a href="/aa">aa meetings</a></li><li><a href="/temperature">temp sensor</a></li><li><a href="/processblog">process blog</a></li></ul>');
+    res.send('<h3>SM Code demo site</h3><ul><li><a href="/aa">aa meetings</a></li><li><a href="/temperature">temp sensor</a></li><li><a href="/processblog">process blog</a></li></ul>');
 });
 
 // respond to requests for /aa
@@ -88,11 +88,9 @@ app.get('/aa', function(req, res) {
     const client = new Pool(db_credentials);
 
     // SQL query 
-    var thisQuery = `SELECT GeoCoord.latlong, json_agg(json_build_object('loc', Building_name, 'address', Address, 'time', EndTime, 'name', Meeting_name, 'day', WeekDay, 'types', Meeting_typeName, 'shour', StartTime)) as meetings
-                 FROM aalocations 
-                 WHERE day = ` + dayy + 'and shour >= ' + hourr +
-        `GROUP BY GeoCoord.latlong
-                 ;`;
+    // var thisQuery = `SELECT *, json_agg(json_build_object('loc', building_name, 'address', address, 'time', endtime, 'name', meeting_name, 'day', weekday, 'types', meeting_typename, 'shour', starttime)) as meetings FROM aalocations WHERE day = ` + dayy + 'and shour >= ' + hourr +
+    //     `GROUP BY geocoord.latlong;`;
+    var thisQuery = `SELECT * FROM aalocations`;
 
     client.query(thisQuery, (qerr, qres) => {
         if (qerr) { throw qerr }
@@ -106,6 +104,29 @@ app.get('/aa', function(req, res) {
     });
 });
 
+
+app.get('/temperature', function(req, res) {
+
+    // Connect to the AWS RDS Postgres database
+    const client = new Pool(db_credentials);
+
+    // SQL query 
+    var q = `SELECT EXTRACT(DAY FROM sensortime) as sensorday,
+             AVG(tempvalue::int) as num_obs
+             FROM sensorData
+             GROUP BY sensorday
+             ORDER BY sensorday;`;
+
+    client.connect();
+    client.query(q, (qerr, qres) => {
+        if (qerr) { throw qerr }
+        else {
+            res.end(template({ sensordata: JSON.stringify(qres.rows) }));
+            client.end();
+            console.log('1) responded to request for sensor graph');
+        }
+    });
+});
 
 // serve static files in /public
 app.use(express.static('public'));
